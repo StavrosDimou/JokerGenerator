d5ad5e48174bc972d1951fa788a4e997c892def9
StavrosDimou <dsocofficial@gmail.com>
2016-09-14 13:33:24 +0300

    Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+// ###############################################################################
+// #                     Δημιουργήθηκε από τον Σταύρο Δήμου                      #
+// #                        GNU - General Public License                         #
+// #                                  © 2016                                     #
+// ###############################################################################
+
+namespace Joker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProtosArithmos = new System.Windows.Forms.TextBox();
+            this.DefterosArithmos = new System.Windows.Forms.TextBox();
+            this.TritosArithmos = new System.Windows.Forms.TextBox();
+            this.TetartosArithmos = new System.Windows.Forms.TextBox();
+            this.PemptosArithmos = new System.Windows.Forms.TextBox();
+            this.labelArithmoi = new System.Windows.Forms.Label();
+            this.labelJoker = new System.Windows.Forms.Label();
+            this.JokerArithmos = new System.Windows.Forms.TextBox();
+            this.NeoDeltioButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.πληροφορίεςToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.InfoButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(468, 171);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // ProtosArithmos
+            // 
+            this.ProtosArithmos.Location = new System.Drawing.Point(89, 182);
+            this.ProtosArithmos.Name = "ProtosArithmos";
+            this.ProtosArithmos.ReadOnly = true;
+            this.ProtosArithmos.Size = new System.Drawing.Size(26, 20);
+            this.ProtosArithmos.TabIndex = 2;
+            // 
+            // DefterosArithmos
+            // 
+            this.DefterosArithmos.Location = new System.Drawing.Point(121, 182);
+            this.DefterosArithmos.Name = "DefterosArithmos";
+            this.DefterosArithmos.ReadOnly = true;
+            this.DefterosArithmos.Size = new System.Drawing.Size(26, 20);
+            this.DefterosArithmos.TabIndex = 3;
+            // 
+            // TritosArithmos
+            // 
+            this.TritosArithmos.Location = new System.Drawing.Point(153, 182);
+            this.TritosArithmos.Name = "TritosArithmos";
+            this.TritosArithmos.ReadOnly = true;
+            this.TritosArithmos.Size = new System.Drawing.Size(26, 20);
+            this.TritosArithmos.TabIndex = 4;
+            // 
+            // TetartosArithmos
+            // 
+            this.TetartosArithmos.Location = new System.Drawing.Point(184, 182);
+            this.TetartosArithmos.Name = "TetartosArithmos";
+            this.TetartosArithmos.ReadOnly = true;
+            this.TetartosArithmos.Size = new System.Drawing.Size(26, 20);
+            this.TetartosArithmos.TabIndex = 5;
+            // 
+            // PemptosArithmos
+            // 
+            this.PemptosArithmos.Location = new System.Drawing.Point(216, 182);
+            this.PemptosArithmos.Name = "PemptosArithmos";
+            this.PemptosArithmos.ReadOnly = true;
+            this.PemptosArithmos.Size = new System.Drawing.Size(26, 20);
+            this.PemptosArithmos.TabIndex = 6;
+            // 
+            // labelArithmoi
+            // 
+            this.labelArithmoi.AutoSize = true;
+            this.labelArithmoi.Location = new System.Drawing.Point(12, 188);
+            this.labelArithmoi.Name = "labelArithmoi";
+            this.labelArithmoi.Size = new System.Drawing.Size(47, 13);
+            this.labelArithmoi.TabIndex = 7;
+            this.labelArithmoi.Text = "Αριθμοί:";
+            // 
+            // labelJoker
+            // 
+            this.labelJoker.AutoSize = true;
+            this.labelJoker.Location = new System.Drawing.Point(12, 211);
+            this.labelJoker.Name = "labelJoker";
+            this.labelJoker.Size = new System.Drawing.Size(36, 13);
+            this.labelJoker.TabIndex = 8;
+            this.labelJoker.Text = "Joker:";
+            // 
+            // JokerArithmos
+            // 
+            this.JokerArithmos.Location = new System.Drawing.Point(89, 208);
+            this.JokerArithmos.Name = "JokerArithmos";
+            this.JokerArithmos.ReadOnly = true;
+            this.JokerArithmos.Size = new System.Drawing.Size(26, 20);
+            this.JokerArithmos.TabIndex = 9;
+            // 
+            // NeoDeltioButton
+            // 
+            this.NeoDeltioButton.Location = new System.Drawing.Point(379, 180);
+            this.NeoDeltioButton.Name = "NeoDeltioButton";
+            this.NeoDeltioButton.Size = new System.Drawing.Size(77, 23);
+            this.NeoDeltioButton.TabIndex = 10;
+            this.NeoDeltioButton.Text = "Νέο Δελτίο";
+            this.NeoDeltioButton.UseVisualStyleBackColor = true;
+            this.NeoDeltioButton.Click += new System.EventHandler(this.NeoDeltioButton_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.πληροφορίεςToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(148, 26);
+            // 
+            // πληροφορίεςToolStripMenuItem
+            // 
+            this.πληροφορίεςToolStripMenuItem.Name = "πληροφορίεςToolStripMenuItem";
+            this.πληροφορίεςToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.πληροφορίεςToolStripMenuItem.Text = "Πληροφορίες";
+            this.πληροφορίεςToolStripMenuItem.Click += new System.EventHandler(this.πληροφορίεςToolStripMenuItem_Click);
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.Location = new System.Drawing.Point(379, 208);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.Size = new System.Drawing.Size(75, 23);
+            this.InfoButton.TabIndex = 12;
+            this.InfoButton.Text = "Σχετικά";
+            this.InfoButton.UseVisualStyleBackColor = true;
+            this.InfoButton.Click += new System.EventHandler(this.InfoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 260);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.NeoDeltioButton);
+            this.Controls.Add(this.JokerArithmos);
+            this.Controls.Add(this.labelJoker);
+            this.Controls.Add(this.labelArithmoi);
+            this.Controls.Add(this.PemptosArithmos);
+            this.Controls.Add(this.TetartosArithmos);
+            this.Controls.Add(this.TritosArithmos);
+            this.Controls.Add(this.DefterosArithmos);
+            this.Controls.Add(this.ProtosArithmos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Joker Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ProtosArithmos;
+        private System.Windows.Forms.TextBox DefterosArithmos;
+        private System.Windows.Forms.TextBox TritosArithmos;
+        private System.Windows.Forms.TextBox TetartosArithmos;
+        private System.Windows.Forms.TextBox PemptosArithmos;
+        private System.Windows.Forms.Label labelArithmoi;
+        private System.Windows.Forms.Label labelJoker;
+        private System.Windows.Forms.TextBox JokerArithmos;
+        private System.Windows.Forms.Button NeoDeltioButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem πληροφορίεςToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button InfoButton;
+    }
+}
